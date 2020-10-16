789c2a278eef9d40fa1d7dc6c80db82dbd38e4e0
Dan <rodger.swan@gmail.com>
2020-10-17 10:38:40 +1100

    Bulma components added

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import { Columns } from 'react-bulma-components';
+import { Card, Content } from 'react-bulma-components';
 import './styles.css';
 
 export default function Cards(props) {
   return (
-    <Columns className="features">
-      <Columns.Column className="is-4">
-        <div className="card is-shady" key={props.id}>
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img src={props.image} alt="project logo" id="myMVP-btn" />
-            </figure>
-          </div>
-          <div className="card-content">
-            <div className="content">
-              <h4>{props.title}</h4>
-              <p>{props.about}</p>
-              <p>
-                <a
-                  href={props.deployed}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Deployed App
-                </a>
-                |
-                <a
-                  href={props.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github repo
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </Columns.Column>
-    </Columns>
+    // <Columns className="features">
+    //   <Columns.Column className="is-4">
+    <Card className="card is-shady" key={props.id}>
+      <Card.Image size="4by3" src={props.image} alt="project logo" />
+      <Card.Content className="card-content">
+        <Content className="content">
+          <h4>{props.title}</h4>
+          <p>{props.about}</p>
+        </Content>
+      </Card.Content>
+      <Card.Footer>
+        <Card.Footer.Item
+          renderAs="a"
+          href={props.deployed}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Deployed App
+        </Card.Footer.Item>
+
+        <Card.Footer.Item
+          renderAs="a"
+          href={props.github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github repo
+        </Card.Footer.Item>
+      </Card.Footer>
+    </Card>
+    //   </Columns.Column>
+    // </Columns>
   );
 }
