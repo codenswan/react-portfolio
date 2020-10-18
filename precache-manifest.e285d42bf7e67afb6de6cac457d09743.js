07f82e948069aa624e0237603e9a4b5a462a8221
Dan <rodger.swan@gmail.com>
2020-10-19 10:57:19 +1100

    Updates

---
+++ precache-manifest.e285d42bf7e67afb6de6cac457d09743.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1271ef05dbd79a5c228292af7671aff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd108fc6479c1a14c76",
+    "url": "/static/css/2.0111e717.chunk.css"
+  },
+  {
+    "revision": "af06c40b0ec03461068b",
+    "url": "/static/css/main.08c8e672.chunk.css"
+  },
+  {
+    "revision": "0bd108fc6479c1a14c76",
+    "url": "/static/js/2.3f22b5af.chunk.js"
+  },
+  {
+    "revision": "6c5833db93cd7c0f813219a8bc75480b",
+    "url": "/static/js/2.3f22b5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af06c40b0ec03461068b",
+    "url": "/static/js/main.7322689a.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "eaa182226a38b1edb26dae323d2b7238",
+    "url": "/static/media/Tech-Base.eaa18222.png"
+  },
+  {
+    "revision": "0d01f30a420e13debbd60a94616b6be0",
+    "url": "/static/media/circle-cropped.0d01f30a.png"
+  }
+]);
